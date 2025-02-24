bbf6f9d532bd435d27f46afcd7d25df133719fd7
anilburakc <swdanilburak@gmail.com>
2025-02-24 18:09:11 +0300

    component
    componentler olusturuldu ve routing ile app.jsx baglandi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react'
 import './App.css'
+import Home from './Component/Home'
+import Order from './Component/Order'
+import Succsess from './Component/Succsess'
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min'
 
 function App() {
 
   return (
     <>
-    
+    <Switch>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route exact path='/order'>
+        <Order />
+      </Route>
+      <Route exact path='/succsess'>
+        <Succsess />
+      </Route>
+    </Switch>
+
     </>
   )
 }
