6c73f0c9e051ab0e36d62c3c49c537e865b8b54a
anilburakc <swdanilburak@gmail.com>
2025-02-26 00:13:20 +0300

    order css updates

--- src/Component/Order.jsx
+++ src/Component/Order.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 import OrderCheck from "./OrderCheck";
 
-export default function Order({formData, handleChange}){
+export default function Order({ formData, handleChange }) {
     const ekler = [
         {
             label: 'Pepperoni',
@@ -18,7 +18,7 @@ export default function Order({formData, handleChange}){
         {
             label: 'Sosis',
             value: 'sosis',
-        },        
+        },
         {
             label: 'Misir',
             value: 'misir',
@@ -26,7 +26,7 @@ export default function Order({formData, handleChange}){
         {
             label: 'Sucuk',
             value: 'sucuk',
-        },        
+        },
         {
             label: 'Kanada Jambonu',
             value: 'kanadaJambonu',
@@ -34,7 +34,7 @@ export default function Order({formData, handleChange}){
         {
             label: 'Ananas',
             value: 'ananas',
-        },        
+        },
         {
             label: 'Tavuk Izgara',
             value: 'tavukIzgara',
@@ -42,7 +42,7 @@ export default function Order({formData, handleChange}){
         {
             label: 'Jalepeno',
             value: 'jalepeno',
-        },        
+        },
         {
             label: 'Kabak',
             value: 'kabak',
@@ -57,91 +57,108 @@ export default function Order({formData, handleChange}){
         },
     ];
 
-    return(
+    return (
         <>
-            <h1>Order</h1>
-            <header>
-                <img src="/images/iteration-1-images/logo.svg"/>
-                <NavLink to='/'>Anasayfa -</NavLink>
-                <NavLink to='/order'> Siparis Olustur</NavLink>
+            <header className="order-header">
+                <img src="/images/iteration-1-images/logo.svg" alt="Logo" />
+                <nav className="header-nav">
+                    <NavLink to='/'>Anasayfa -</NavLink>
+                    <NavLink style={{ fontWeight: 'bold' }} to='/order'> Siparis Olustur</NavLink>
+                </nav>
             </header>
-            <section>
-                <h2>Position Absolute Aci Pizza</h2>
-                <div>
-                    <p style={{fontWeight:'bold'}}>85.50₺</p>
-                    <p>4.9</p>
-                    <p>(200)</p>
-                </div>
-                <p>Frontent Dev olarak hala position:absolute kullaniyorsan bu cok aci 
-                    pizza tam sana gore. Pizza, domates, peynir ve genellikle cesitli diger
-                    malzemelerle kaplanmis, daha sonra geleneksel olarak odun atesinde bir
-                    firinda yuksek sicaklikta pisirilen, genellikle yuvarlak, duzlestirilmis
-                    mayali bugday bazli hamurdan olusan Italyan kokenli lezzetli bir yemektir.
-                    Kucuk bir pizzaya bazen pizzetta denir.
-                </p>
-            </section>
-            <div>
-                <form>
-                    <fieldset className="form-select">
-                        <div className="select-radio">
-                            <legend className="select-legend">Boyut Sec</legend>
-                            <label className="select-radio-item">
-                                <input 
-                                    type="radio"
-                                    onChange={handleChange}
-                                    name="boyutSec"
-                                    value='kucuk'
-                                />
-                                Kucuk
-                            </label>
-                            <label className="select-radio-item">
-                                <input 
-                                    type="radio"
-                                    onChange={handleChange}
-                                    name="boyutSec"
-                                    value='orta'
-                                />
-                                Orta
-                            </label>
-                            <label className="select-radio-item">
-                                <input 
-                                    type="radio"
-                                    onChange={handleChange}
-                                    name="boyutSec"
-                                    value='buyuk'
-                                />
-                                Buyuk
-                            </label>
-                        </div>
-                        <div className="select-option">
-                            <legend className="select-legend">Hamur Sec</legend>
-                            <select>
-                                <option value='ince'>Ince</option>
-                                <option value='normal'>Normal</option>
-                                <option value='kalin'>Kalin</option>
-                            </select>
-                        </div>
+            <main className="order-main">
+                <section className="order-info">
+                    <h2>Position Absolute Aci Pizza</h2>
+                    <div className="order-info-div">
+                        <p style={{ fontWeight: 'bold' }}>85.50₺</p>
+                        <p>4.9</p>
+                        <p>(200)</p>
+                    </div>
+                    <p >Frontent Dev olarak hala position:absolute kullaniyorsan bu cok aci
+                        pizza tam sana gore. Pizza, domates, peynir ve genellikle cesitli diger
+                        malzemelerle kaplanmis, daha sonra geleneksel olarak odun atesinde bir
+                        firinda yuksek sicaklikta pisirilen, genellikle yuvarlak, duzlestirilmis
+                        mayali bugday bazli hamurdan olusan Italyan kokenli lezzetli bir yemektir.
+                        Kucuk bir pizzaya bazen pizzetta denir.
+                    </p>
+                </section>
+                <section className="order-form">
+                    <form className="form-content">
+                        <fieldset className="form-select">
+                            <div className="select-radio-item">
+                                <legend className="select-legend">Boyut Sec</legend>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        onChange={handleChange}
+                                        name="boyutSec"
+                                        value='kucuk'
+                                    />
+                                    Kucuk
+                                </label>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        onChange={handleChange}
+                                        name="boyutSec"
+                                        value='orta'
+                                    />
+                                    Orta
+                                </label>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        onChange={handleChange}
+                                        name="boyutSec"
+                                        value='buyuk'
+                                    />
+                                    Buyuk
+                                </label>
 
-                    </fieldset>
-                    <fieldset className="form-ek">
-                    <legend>Ek Malzemeler</legend>
-                    <p className="form-ek-info">en fazla 10 malzeme sec</p>
-                        <div className="checkbox-container">
-                        {ekler.map((ek)=>(
-                        <OrderCheck 
-                            key={ek.value}
-                            changeFunc={handleChange}
-                            isChecked={formData.ekMalzemeler.includes(ek.value)}
-                            fieldName="ekMalzemeler"
-                            value={ek.value}
-                            label={ek.label}
-                        />
-                        ))}
-                        </div>
-                    </fieldset>
-                </form>
-            </div>
-            <footer>siparis toplami</footer>
+                            </div>
+
+                            <div className="select-option-item">
+                                <legend className="select-legend">Hamur Sec</legend>
+                                <select>
+                                    <option value='ince'>Ince</option>
+                                    <option value='normal'>Normal</option>
+                                    <option value='kalin'>Kalin</option>
+                                </select>
+                            </div>
+                        </fieldset>
+                        <fieldset className="form-ekMalzemeler">
+                            <legend>Ek Malzemeler</legend>
+                            <p className="form-ek-info">en fazla 10 malzeme sec</p>
+                            <div className="checkbox-container">
+                                {ekler.map((ek) => (
+                                    <OrderCheck
+                                        key={ek.value}
+                                        changeFunc={handleChange}
+                                        isChecked={formData.ekMalzemeler.includes(ek.value)}
+                                        fieldName="ekMalzemeler"
+                                        value={ek.value}
+                                        label={ek.label}
+                                    />
+                                ))}
+                            </div>
+                        </fieldset>
+
+                        <fieldset className="isim-not">
+                            <label htmlFor="isim">Isminizi giriniz:</label>
+                            <input
+                                type="text"
+                                id="isim"
+                                name="isim"
+                            />
+                            <label htmlFor="not">Siparis Notu</label>
+                            <textarea id="not" placeholder='Siparisine eklemek istedigin bir not var mi?'></textarea>
+                        </fieldset>
+
+                    </form>
+                </section>
+                <footer>siparis toplami</footer>
+
+            </main>
         </>
     );
 }
